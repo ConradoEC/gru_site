bf400031dff441c47e4c7a9ed4b4f311b9607634
ConradoEC <113607331+ConradoEC@users.noreply.github.com>
2025-11-19 11:33:56 -0300

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -6,5 +6,6 @@ const botao = document.getElementById("botao");
 botao.addEventListener("click", (e) => {
     e.preventDefault();
     
-    window.location.href = "http://127.0.0.1:5500/arquivos/lixeira.html";
+    window.location.href = "https://conradoec.github.io/gru_site/lixeira.html";
 })
+
